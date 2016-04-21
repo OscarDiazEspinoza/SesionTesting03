dceb9b73259d5a55ce95b153d88fd162dc39a3a5
oscar diaz espinoza <oscar diaz espinoza>
2016-04-20 21:46:35 -0300

    https://github.com/OscarDiazEspinoza/SesionTesting03.git

--- src/test/java/cl/ubb/agil/ProductoTest.java
+++ src/test/java/cl/ubb/agil/ProductoTest.java
@@ -1,5 +1,32 @@
 package cl.ubb.agil;
 
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cl.ubb.agil.Producto;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class ProductoTest {
+	
+private Producto producto;
+	
+
+	@Before
+	public void setup() throws Exception {
+		producto = new Producto(null, 0, 0);
+	}
+	
+	@Test
+	
+	public void stocknotnull (){
+		/* Arrange */
+		
+		/* Act */	
+	
+		/* Assert */
+		
+	}
 
 }
